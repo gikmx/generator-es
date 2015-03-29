81cfc1d485158d1aa08c25e01332589101464356
Héctor Menéndez Rivera <etor.mx@gmail.com>
2015-03-29 05:33:16 -0600

    Removed lazypipe,  ignoring source files, added travis, added coveralls.

--- .conf/gulp.js
+++ .conf/gulp.js
@@ -13,19 +13,19 @@ const Source = require('gulp-sourcemaps');
 const Babel  = require('gulp-babel');
 const Mocha  = require('gulp-spawn-mocha');
 const Del    = require('del');
-const Pipes  = require('lazypipe');
 
 
 //------------------------------------------------------------------------- PATHS & ROUTES
 
 const Dir = new String(Path.resolve(Path.join(__dirname, '..')));
 
-for (let dir of ['src', 'test', 'build', '.conf']) Object.defineProperty(Dir, dir, {
-	value        : Path.join(String(Dir), dir),
-	writable     : false,
-	enumerable   : true,
-	configurable : false
-});
+for (let dir of ['src', 'test', 'build', '.conf', 'coverage'])
+	Object.defineProperty(Dir, dir, {
+		value        : Path.join(String(Dir), dir),
+		writable     : false,
+		enumerable   : true,
+		configurable : false
+	});
 
 const Route = {
 	src  : [Path.join(Dir.src, '/**/*.js')],
@@ -59,55 +59,51 @@ Config.babel = {
 Config.mocha = {
 	ui        : 'bdd',
 	bail      : true,
-	reporter  : 'mocha-unfunk-reporter',
 	require   : Path.join(Dir['.conf'], 'chai'),
+	reporter  : 'mocha-unfunk-reporter',
 	compilers : 'js:babel/register',
-	istanbul  : { dir: Path.join(Dir.build, 'coverage') }
+	istanbul  : {
+		dir: Path.join(Dir.toString(), 'coverage')
+	}
 };
-
-
-//------------------------------------------------------------------------- ERROR HANDLING
-
-const onError = function(){ process.exit(1); };
 
 
 //---------------------------------------------------------------------------------- TASKS
 
+Gulp.task('clean', function(callback){
+	Del([Dir.build, Dir.coverage], callback);
+});
+
+Gulp.task('lint-self', ()=>
+	Gulp.src(__filename)
+		.pipe(Lint(Config.lint.src))
+		.pipe(Lint.format())
+		.pipe(Lint.failOnError())
+);
+
+Gulp.task('lint-test', ['lint-self'], ()=>
+	Gulp.src(Route.test)
+		.pipe(Lint(Config.lint.test))
+		.pipe(Lint.format())
+		.pipe(Lint.failOnError())
+);
+
+Gulp.task('lint', ['lint-self'], ()=>
+	Gulp.src(Route.src)
+		.pipe(Lint(Config.lint.src))
+		.pipe(Lint.format())
+		.pipe(Lint.failOnError())
+);
+
+Gulp.task('test', ['lint-test'], ()=>
+	Gulp.src(Route.test)
+		.pipe(Mocha(Config.mocha))
+);
+
 Gulp.task('build', ['clean', 'lint', 'test'], function(){
 	return Gulp.src(Route.src)
-		.pipe(Source.init().on('error', onError))
+		.pipe(Source.init())
 		.pipe(Babel(Config.babel))
 		.pipe(Source.write()) // inline sourcemaps
 		.pipe(Gulp.dest(Dir.build));
 });
-
-Gulp.task('test', ['lint-self', 'lint-test'], function(){
-	return Gulp.src(Route.test)
-		.pipe(Mocha(Config.mocha).on('error', onError));
-});
-
-Gulp.task('clean', function(callback){
-	Del([Dir.build], callback);
-});
-
-const PipeLint = Pipes()
-	.pipe(Lint.format)
-	.pipe(Lint.failOnError);
-
-Gulp.task('lint-self', ()=>
-	Gulp.src(__filename)
-		.pipe(Lint(Config.lint.src).on('error', onError))
-		.pipe(PipeLint())
-);
-
-Gulp.task('lint-test', ()=>
-	Gulp.src(Route.test)
-		.pipe(Lint(Config.lint.test).on('error', onError))
-		.pipe(PipeLint())
-);
-
-Gulp.task('lint', ['lint-self', 'lint-test'], ()=>
-	Gulp.src(Route.src)
-		.pipe(Lint(Config.lint.src).on('error', onError))
-		.pipe(PipeLint())
-);
