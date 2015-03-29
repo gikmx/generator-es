81c9157e41e04a6dbdd5bddf4b201b788edc7832
Héctor Menéndez Rivera <etor.mx@gmail.com>
2015-03-29 04:19:56 -0600

    Enabled mocha and chai support vía spawn, istannbul enabled.

--- .conf/gulp.js
+++ .conf/gulp.js
@@ -1,37 +1,33 @@
 'use strict';
 
-////////////////////////////// NODE MODULES
+///////////////////////////////////////////////////////////////////////////// NODE MODULES
 
 const Path  = require('path');
 
 
-////////////////////////////// NPM MODULES
+////////////////////////////////////////////////////////////////////////////// NPM MODULES
 
 const Gulp   = require('gulp');
-const Util   = require('gulp-util');
 const Lint   = require('gulp-eslint');
 const Source = require('gulp-sourcemaps');
 const Babel  = require('gulp-babel');
-const Mocha  = require('gulp-mocha');
+const Mocha  = require('gulp-spawn-mocha');
 const Extend = require('extend');
 const Del    = require('del');
 const Pipes  = require('lazypipe');
 
-const Config = {};
 
-Config.dir = {
-	configurable : false,
-	enumerable   : true,
-	writable     : true
-};
-
-////////////////////////////// PATH SETUP
+////////////////////////////////////////////////////////////////////////////////// ROUTING
 
 const Dir = new String(Path.resolve(Path.join(__dirname, '..')));
 
-for (let dir of ['src', 'test', 'build']){
+for (let dir of ['src', 'test', 'build', '.conf']){
 	let prop = { value:Path.join(String(Dir), dir) };
-	Object.defineProperty(Dir, dir, Extend(prop, Config.dir));
+	Object.defineProperty(Dir, dir, Extend(prop, {
+		configurable : false,
+		enumerable   : true,
+		writable     : false
+	}));
 }
 
 const Route = {
@@ -39,15 +35,18 @@ const Route = {
 	test : [Path.join(Dir.test, '/**/*.js')]
 };
 
-////////////////////////////// CONFIGURATION
+
+//////////////////////////////////////////////////////////////////////////// CONFIGURATION
+
+const Config = {};
 
 Config.lint = {
 
-	src:{
+	src: {
 		useEslintrc : true
 	},
 
-	test:{
+	test: {
 		rulesPath   : [Dir.test],
 		useEslintrc : true
 	}
@@ -60,29 +59,35 @@ Config.babel = {
 	comments      : false
 };
 
-
 Config.mocha = {
-	ui       : 'bdd',
-	reporter : 'mocha-unfunk-reporter',
-	require  : './chai'
+	ui        : 'bdd',
+	bail      : true,
+	reporter  : 'mocha-unfunk-reporter',
+	require   : Path.join(Dir['.conf'], 'chai'),
+	compilers : 'js:babel/register',
+	istanbul  : { dir: Path.join(Dir.build, 'coverage') }
 };
 
 
-////////////////////////////// TASKS
+/////////////////////////////////////////////////////////////////////////// ERROR HANDLING
 
-Gulp.task('test', ['lint-self', 'lint-test'], function(){
-	return Gulp.src(Route.test)
-		.pipe(Mocha(Config.mocha));
-});
+const onError = function(){ process.exit(1); };
 
-Gulp.task('build', ['lint', 'clean'], function(){
+
+//////////////////////////////////////////////////////////////////////////////////// TASKS
+
+Gulp.task('build', ['clean', 'lint', 'test'], function(){
 	return Gulp.src(Route.src)
-		.pipe(Source.init().on('error', Util.log))
+		.pipe(Source.init().on('error', onError))
 		.pipe(Babel(Config.babel))
 		.pipe(Source.write()) // inline sourcemaps
 		.pipe(Gulp.dest(Dir.build));
 });
 
+Gulp.task('test', ['lint-self', 'lint-test'], function(){
+	return Gulp.src(Route.test)
+		.pipe(Mocha(Config.mocha).on('error', onError));
+});
 
 Gulp.task('clean', function(callback){
 	Del([Dir.build], callback);
@@ -93,13 +98,19 @@ const PipeLint = Pipes()
 	.pipe(Lint.failOnError);
 
 Gulp.task('lint-self', ()=>
-	Gulp.src(__filename).pipe(Lint(Config.lint.src)).pipe(PipeLint())
+	Gulp.src(__filename)
+		.pipe(Lint(Config.lint.src).on('error', onError))
+		.pipe(PipeLint())
 );
 
 Gulp.task('lint-test', ()=>
-	Gulp.src(Route.test).pipe(Lint(Config.lint.test)).pipe(PipeLint())
+	Gulp.src(Route.test)
+		.pipe(Lint(Config.lint.test).on('error', onError))
+		.pipe(PipeLint())
 );
 
 Gulp.task('lint', ['lint-self', 'lint-test'], ()=>
-	Gulp.src(Route.src).pipe(Lint(Config.lint.src)).pipe(PipeLint())
+	Gulp.src(Route.src)
+		.pipe(Lint(Config.lint.src).on('error', onError))
+		.pipe(PipeLint())
 );
